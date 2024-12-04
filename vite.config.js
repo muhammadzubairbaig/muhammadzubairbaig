2799ad7319c6e380ec89a3ccd4f1807ef08d0849
Muhammad Zubair Baig <zubair@autoleap.com>
2024-12-05 03:22:01 +0500

    updated vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  output: 'export', // Ensures the app can be statically exported
-  assetPrefix: './', // Adjusts asset paths for GitHub Pages
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Set limit to 1MB or any size you prefer
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-});
+})
